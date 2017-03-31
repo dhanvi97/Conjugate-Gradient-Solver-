16dbc0702fbb9ac0e99214487b2669f5a3a2b2ed
Anant Joshi <anantjoshi67iitb@gmail.com>
2017-03-31 17:14:16 +0530

    Tried examples, recorded no. of iterations

--- GD_implement.py
+++ GD_implement.py
@@ -5,16 +5,18 @@ def linear_iter(A, x, b):
 	return np.subtract(b,np.matmul(A,x))
 
 def alpha_calc(r, A):
+	'''The 0.1 factor is for coverting to decimal, otherwise returns int later'''
 	return (np.matmul(np.transpose(r),r)*0.1)/(np.matmul(np.transpose(r),np.matmul(A,r))*0.1)
 
 if __name__ == '__main__':
-	A = np.matrix('4 1;1 3')
-	b = np.matrix('1;2')
+	A = np.matrix('3 2;2 6')
+	b = np.matrix('2;-8')
 	x_0 = np.matrix('0;0')
 	x_i = x_0
 	r_0 = linear_iter(A,x_0,b)
 	r_i = r_0
-	epsilon = 0.00000000000001
+	i = 0
+	epsilon = 0.00000001
 	while (LA.norm(r_i) >= epsilon):
 		r_i = linear_iter(A,x_i,b)
 		'''print(r_i)'''
@@ -23,4 +25,8 @@ if __name__ == '__main__':
 		x_i = np.add(x_i,np.multiply(alpha,r_i))
 		'''print(x_i)'''
 		r_i = np.subtract(r_i,np.multiply(alpha,np.matmul(A,r_i)))
+		i += 1
 	print(x_i)
+	print(i)
+	'''Takes 63 iterations for 3x3 example'''
+	'''Takes 35 iterations for 2x2 example'''
